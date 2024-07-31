ccd0d326f7a8d58aecc15f3984690c7b152e3788
Oğuzhan Varlı <106310128+oguzhanvarli@users.noreply.github.com>
2024-07-31 19:08:53 +0300

    30 Temmuz günsonu

---
+++ native/first/App.jsx
@@ -0,0 +1,17 @@
+import { View, Text, Button } from 'react-native'
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [counter, setCounter] = useState(0)
+
+  return (
+    <View>
+      <Text style={{fontSize: 40, color: "#f21212", textAlign: "center"}}>{counter}</Text>
+      <Button title='Azalt' onPress={() => setCounter(counter - 1)} color={"#f99"}></Button>
+      <Button title='Arttır' onPress={() => setCounter(counter + 1)} color={"red"} />
+    </View>
+  )
+}
+
+export default App
